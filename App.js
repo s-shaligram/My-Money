5e9409ed73655aab7ec49cc72863cf8a24763d67
Sameer_Shaligram <s_shaligram@fanshaweonline.ca>
2023-04-15 16:37:11 -0400

    Initial code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My-Money by US</Text>
+      <Text>My-Money</Text>
       <StatusBar style="auto" />
     </View>
   );
