4b07073f3fe8663d9db39ea1daef0d50b2f254da
Sameer_Shaligram <s_shaligram@fanshaweonline.ca>
2023-04-15 16:35:10 -0400

    Initial changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My-Money</Text>
+      <Text>My-Money by US</Text>
       <StatusBar style="auto" />
     </View>
   );
