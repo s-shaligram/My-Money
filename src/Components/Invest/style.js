04237854bf890054ddd301f43118cb97b535f917
Sameer_Shaligram <s_shaligram@fanshaweonline.ca>
2023-04-15 22:32:37 -0400

    icon and invest code

---
+++ src/Components/Invest/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  
+  
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    marginTop: 5,
+  },
+  
+});
+export default styles;
